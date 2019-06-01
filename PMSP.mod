79807627fd1b5f313bd7ef665cb43d85f095b713
Diego Dimer <diego.dimer@ufrgs.br>
2019-06-01 18:37:49 -0300

    transf instancia e inicio da formulacao

---
+++ PMSP.mod
@@ -0,0 +1,51 @@
+# Dados
+# Jobs
+set I; # i = 1, ... n;
+set J; # j = 0, ... n;
+
+# Máquinas
+set K;
+
+# Tempo de processamento em cada máquina
+param G {i in I, j in J, k in K};
+param V; # valor muito grande 
+
+
+# Variáveis
+# tempo em que o job j foi completo
+var c {j in J} >= 0 integer;
+# job com maior tempo para completar
+var cmax >= 0 integer;
+# 1 se job j é escalonado diretamente depois do job i na máquina k, 0 caso contrário
+var x {i in J, j in J, k in K} binary;
+
+# Função objetivo.
+minimize tempoMaior: cmax;
+
+# todos os jobs tem que ser feitos
+s.t. r1 {j in J} : ( sum {i in I: i!=j} (sum {k in K}(x[i,j,k]) ) ) <= 1;
+s.t. r12 {j in J} : -1 * ( sum {i in I: i!=j} (sum {k in K}(x[i,j,k]) ) ) <= -1;
+
+#todo job é precedido e sucedido por outro job
+s.t. r2 {h in J, k in K}: (sum {i in I: i!=h} x[i,h,k]) - (sum {j in J: j!=h} x[h,j,k]) <= 0;
+s.t. r21 {h in J, k in K}: -1 * (sum {i in I: i!=h} x[i,h,k]) - (sum {j in J: j!=h} x[h,j,k]) <= 0;
+
+# cada job é o tempo dele ser completo + o tempo do anterior ser completo
+s.t. r3 {i in I, j in J}: c[i] + (sum {k in K} (x[i,j,k] * G[i,j,k])) + V * ( sum {k in K} (x[i,j,k] - 1)) <= c[j];
+
+# máximo
+s.t. r4 {j in J}: c[j] <= cmax;
+
+# cada máquina começa com no max 1 job
+s.t. r5 {k in K}: (sum {j in J} x[0,j,k]) <= 1; 
+s.t. r51 {k in K}: -1 * (sum {j in J} x[0,j,k]) <= -1; 
+
+# cada máquina começa com no max 1 job
+s.t. r7 {k in K}: (sum {j in J} x[j,0,k]) <= 1; 
+s.t. r71 {k in K}: -1 * (sum {j in J} x[j,0,k]) <= -1; 
+
+#dummy job (pra soma no r3 começar em 0+t1)
+s.t. r6 : c[0] <= 0;
+s.t. r61: -1 * c[0] <= 0;
+
+end; 
---
+++ transforma_instancia/pmsp-reader.cpp
@@ -0,0 +1,192 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <limits>
+
+class PmspInstance {
+public:
+   PmspInstance(const char *fname);
+   virtual ~PmspInstance();
+
+   int getNumMachines() const;
+   int getNumJobs() const;
+
+   int getProcTime(int job, int machine) const;
+   int getSetupTime(int predJob, int succJob, int machine) const;
+   int getGijk(int predJob, int succJob, int machine) const;
+
+   const char *getName() const;
+   void writeFile(const char *fname) const;
+   void writeFormatGLPK(const char *fname) const;
+
+protected:
+   const std::string m_name;
+   int m_num;
+   int m_numMachines, m_numJobs;
+   std::vector <std::vector <int>> m_procTimes; // [job][machine]
+   std::vector <std::vector <std::vector <int>>> m_setupTimes; // [pred job][succ job][machine]
+};
+
+PmspInstance::PmspInstance(const char* fname): m_name(fname) {
+   std::ifstream fid(fname);
+   if (!fid) {
+      std::cout << "Instance file could not be open to read.\n";
+      std::abort();
+   }
+
+   fid >> m_num >> m_numMachines >> m_numJobs;
+
+   m_procTimes.resize(m_numJobs);
+   m_setupTimes.resize(m_numJobs);
+   for (int i = 0; i < m_numJobs; ++i) {
+      m_procTimes[i].resize(m_numMachines, std::numeric_limits<int>::max());
+      m_setupTimes[i].resize(m_numJobs);
+      for (int j = 0; j < m_numJobs; ++j) {
+         m_setupTimes[i][j].resize(m_numMachines, std::numeric_limits<int>::max());
+      }
+   }
+
+   for (int i = 0; i < m_numJobs; ++i) {
+      for (int j = 0; j < m_numMachines; ++j) {
+         fid >> m_procTimes[i][j];
+      }
+   }
+
+   for (int k = 0; k < m_numMachines; ++k) {
+      for (int i = 0; i < m_numJobs; ++i) {
+         for (int j = 0; j < m_numJobs; ++j) {
+            fid >> m_setupTimes[i][j][k];
+         }
+      }
+   }
+
+}
+
+PmspInstance::~PmspInstance() {
+   // Empty
+}
+
+int PmspInstance::getNumMachines() const {
+   return m_numMachines;
+}
+
+int PmspInstance::getNumJobs() const {
+   return m_numJobs;
+}
+
+int PmspInstance::getProcTime(int job, int machine) const {
+   return m_procTimes[job][machine];
+}
+
+int PmspInstance::getSetupTime(int predJob, int succJob, int machine) const {
+   return m_setupTimes[predJob][succJob][machine];
+}
+
+int PmspInstance::getGijk(int predJob, int succJob, int machine) const {
+   return getProcTime(succJob, machine) + getSetupTime(predJob, succJob, machine);
+}
+
+const char *PmspInstance::getName() const {
+   return m_name.c_str();
+}
+
+void PmspInstance::writeFile(const char* fname) const {
+   std::ofstream fid(fname);
+   if (!fid) {
+      std::cout << "Output file could not be open to write.\n";
+      std::abort();
+   }
+
+   fid << m_num << '\n' << m_numMachines << '\n' << m_numJobs << "\n\n";
+
+   for (int i = 0; i < m_numJobs; ++i) {
+      for (int j = 0; j < m_numMachines; ++j) {
+         fid << m_procTimes[i][j] << ' ';
+      }
+      fid << '\n';
+   }
+   fid << "\n";
+
+   for (int k = 0; k < m_numMachines; ++k) {
+      for (int i = 0; i < m_numJobs; ++i) {
+         for (int j = 0; j < m_numJobs; ++j) {
+            fid << m_setupTimes[i][j][k] << ' ';
+         }
+         fid << "\n";
+      }
+      fid << "\n";
+   }
+}
+
+
+void PmspInstance::writeFormatGLPK(const char* fname) const
+{
+    std::ofstream fid(fname);
+    if (!fid)
+    {
+        std::cout << "Output file could not be open to write.\n";
+        std::abort();
+    }
+    // Monta set I;
+    fid << "set I :=";
+    for (int i=1; i<=m_numJobs; i++)
+    {
+        fid << " " << i ;
+    }
+    fid << ";" << "\n";
+
+    // Monta set J;
+    fid << "set J :=";
+    for (int i=0; i<=m_numJobs; i++)
+    {
+        fid << " " << i ;
+    }
+    fid << ";" << "\n";
+
+     // Monta set K;
+    fid << "set K :=";
+    for (int i=1; i<=m_numMachines; i++)
+    {
+        fid << " " << i ;
+    }
+    fid << ";" << "\n";
+
+    fid <<"param V := 999999999;" << "\n";
+
+    fid << "\n";
+    fid << "param G :=\n";
+    // param G {i in I, j in J, k in K};
+
+
+    for(int i=0; i < m_numJobs; i++)
+    {
+        for(int j=-1; j<m_numJobs; j++)
+        {
+            for(int k=0; k<m_numMachines; k++)
+            {
+                if(j==-1){
+                    fid <<"\t" << i+1 << " " << j+1 << " " << k+1 << " " << getProcTime(i,k) << "\n";
+                }else
+                    fid <<"\t" << i+1 << " " << j+1 << " " << k+1 << " " << getGijk(i,j,k) << "\n";
+            }
+        }
+    }
+    fid << ";" << "\n";
+
+}
+
+
+int main(int argc, char **argv) {
+   if (argc != 2) {
+      std::cout << "Usage: " << argv[0] << " <instance file>\n";
+      return EXIT_FAILURE;
+   }
+   std::cout<<"Starting\n";
+   PmspInstance inst(argv[1]);
+   std::cout<<"Read\n";
+   inst.writeFormatGLPK("pmsp.dat");
+   std::cout<<"Written\n";
+   return EXIT_SUCCESS;
+}
